0ec42fa7a3662e68954f51ce24c5cefa041d7f86
Jingcheng Yang <yjcyxky@163.com>
2023-11-19 10:42:12 -0500

    Improve the scripts for visualizing embeddings.

--- embedding/README.md
+++ embedding/README.md
@@ -6,6 +6,7 @@
 mkdir -p embedding/<MODEL_NAME>
 
 # If your expected model does not support transformers library directly, you can download the model's weights and refer to it by its path.
+# We assume you have downloaded entities.tsv file or generated it by following the instructions in the README.md file in the root directory.
 python embedding/scripts/gen_embeddings.py entities -e graph_data/entities.tsv -m <MODEL_NAME or MODEL_PATH> -o embedding/<MODEL_NAME>/entities_embeddings.tsv
 ```
 
--- embedding/scripts/lib.py
+++ embedding/scripts/lib.py
@@ -1,13 +1,37 @@
 import os
 import pandas as pd
+
 # from sklearn.manifold import TSNE
 from fitsne import FItSNE
 import numpy as np
 import plotly.express as px
 
-allowd_types = ["Gene", "Compound", "Disease", "Symptom", "Pathway", "Anatomy", "Metabolite", "MolecularFunction", "BiologicalProcess", "CellularComponent"]
+allowd_types = [
+    "Gene",
+    "Compound",
+    "Disease",
+    "Symptom",
+    "Pathway",
+    "Anatomy",
+    "Metabolite",
+    "MolecularFunction",
+    "BiologicalProcess",
+    "CellularComponent",
+]
 
-colors = ["#e60049", "#0bb4ff", "#50e991", "#e6d800", "#9b19f5", "#ffa300", "#dc0ab4", "#b3d4ff", "#00bfa0", "#ff6e00"]
+colors = [
+    "#e60049",
+    "#0bb4ff",
+    "#50e991",
+    "#e6d800",
+    "#9b19f5",
+    "#ffa300",
+    "#dc0ab4",
+    "#b3d4ff",
+    "#00bfa0",
+    "#ff6e00",
+]
+
 
 def get_color(type):
     if type in allowd_types:
@@ -15,27 +39,40 @@ def get_color(type):
     else:
         return "#000000"
 
+
 # Load a data file which is a tsv file and the last column is an embedding vector. The embedding vector is a string of numbers separated by pipes.
 
+
 def load_data(file_name):
-    df = pd.read_csv(file_name, sep='\t')
+    df = pd.read_csv(file_name, sep="\t")
     df = df.reset_index(drop=True)
-    df['embedding'] = df['embedding'].apply(lambda x: x.split('|'))
+    df["embedding"] = df["embedding"].apply(lambda x: x.split("|"))
     return df
+
 
 # Visualize the data using t-SNE and show node names on the plot when hovering over the nodes.
 
+
 def get_tsne(df):
-    X = np.array(df['embedding'].tolist())
+    X = np.array(df["embedding"].tolist())
     X = X.astype(np.float64)
     X_2d = FItSNE(X)
     return X_2d
 
-def visualize_entities(df):
-    colors = [get_color(type) for type in set(df['entity_type'])]
-    fig = px.scatter(df, x='x', y='y', hover_name='entity_name', color='entity_type', color_discrete_sequence=colors)
+
+def visualize_entities(df, color_column="entity_type"):
+    colors = [get_color(type) for type in set(df["entity_type"])]
+    fig = px.scatter(
+        df,
+        x="x",
+        y="y",
+        hover_name="entity_name",
+        color=color_column,
+        color_discrete_sequence=colors,
+    )
     return fig
 
-def visualize_relation_types(df):
-    fig = px.scatter(df, x='x', y='y', color='relation_type')
+
+def visualize_relation_types(df, color_column="relation_type"):
+    fig = px.scatter(df, x="x", y="y", color=color_column, hover_name="relation_type")
     return fig
