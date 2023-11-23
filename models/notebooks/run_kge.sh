53dca3e0335c563db3d951bf042e2f4bacdd525f
Jingcheng Yang <yjcyxky@163.com>
2023-11-23 16:32:53 -0500

    Allow to run validation dataset.

--- models/notebooks/run_kge.sh
+++ models/notebooks/run_kge.sh
@@ -12,4 +12,4 @@ if [ -f ${LOG_FILE} ]; then
     exit 1
 fi
 
-DGLBACKEND=pytorch dglke_train --dataset biomedgps --data_path ./data/train --data_files train.tsv valid.tsv test.tsv --format 'raw_udd_hrt' --model_name TransE_l2 --batch_size 2048 --neg_sample_size 256 --hidden_dim 400 --gamma 12.0 --lr 0.1 --max_step 100000 --log_interval 1000 --batch_size_eval 16 -adv --regularization_coef 1.00E-07 --test --gpu 0 --num_proc 7 --neg_sample_size_eval 10000 --async_update --mix_cpu_gpu --save_path ./models 2>&1 | tee ${LOG_FILE}
+DGLBACKEND=pytorch dglke_train --dataset biomedgps --data_path ./data/train --data_files train.tsv valid.tsv test.tsv --format 'raw_udd_hrt' --model_name TransE_l2 --batch_size 2048 --neg_sample_size 256 --hidden_dim 400 --gamma 12.0 --lr 0.1 --max_step 100000 --log_interval 1000 --batch_size_eval 16 -adv --regularization_coef 1.00E-07 --test --valid --gpu 0 --num_proc 7 --neg_sample_size_eval 10000 --async_update --mix_cpu_gpu --save_path ./models 2>&1 | tee ${LOG_FILE}
